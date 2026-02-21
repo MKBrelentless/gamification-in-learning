ffac051d87886f0241300538e50e82b85e15b029
Austine Mukabwa <austinmukabwa02@gmail.com>
2026-02-21 20:57:23 +0300

    Make navbar sticky to remain visible when scrolling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-white shadow-lg sticky top-0 z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center space-x-4">
